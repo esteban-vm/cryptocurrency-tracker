855468b8b8aaa101d61b88af7c2bac711a041f6c
esteban-vm <estebanvm90@outlook.com>
2024-01-27 09:48:13 -0300

    update utils

--- src/components/row.tsx
+++ src/components/row.tsx
@@ -1,6 +1,6 @@
 import type { CoinMarkets } from '@crypto-coffee/coingecko-api/dist/types'
 import type { FC } from 'react'
-import { formatCurrency, formatPercent, attachClass, attachArrow } from '@/utils'
+import * as Utils from '@/utils'
 
 interface RowProps extends CoinMarkets {
   index: number
@@ -15,18 +15,19 @@ const Row: FC<RowProps> = ({
   price_change_percentage_24h: change,
   market_cap: cap,
 }) => {
+  const formattedPrice = Utils.formatCurrency(price)
+  const formattedCap = Utils.formatCurrency(cap)
+  const [formattedChange, className] = Utils.formatPercent(change)
+
   return (
     <tr>
       <th scope='row'>{index}</th>
       <td>
         <img src={image} alt={name} /> {name} ({symbol})
       </td>
-      <td>{formatCurrency(price)}</td>
-      <td className={attachClass(change)}>
-        <span>{attachArrow(change)}</span>
-        {formatPercent(change)}
-      </td>
-      <td>{formatCurrency(cap)}</td>
+      <td>{formattedPrice}</td>
+      <td className={className}>{formattedChange}</td>
+      <td>{formattedCap}</td>
     </tr>
   )
 }
